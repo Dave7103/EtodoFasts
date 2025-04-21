8e4a903f02208acee63d1170d1eba59522d563c0
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-21 20:31:26 +0800

    s

--- database.py
+++ database.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://todofastapi_v2g7_user:erMWTu3PyspcAuQn76vUJKyI5egFmo7f@dpg-d02udcje5dus73c5rdgg-a/todofastapi_v2g7"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
