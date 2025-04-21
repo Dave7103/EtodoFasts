1223b334da29a586922d59446ab3ce0c7856cb15
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-21 20:43:37 +0800

    s

--- main.py
+++ main.py
@@ -33,26 +33,26 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/todos", response_model=list[schemas.ToDoInDB])
+@app.get("/todos", response_model=list[schemas.TodoInDB])
 def read_todos(db: Session = Depends(get_db)):
     return crud.get_todos(db)
 
-@app.get("/todos/{todo_id}", response_model=schemas.ToDoInDB)
+@app.get("/todos/{todo_id}", response_model=schemas.TodoInDB)
 def read_todo(todo_id: int, db: Session = Depends(get_db)):
     todo = crud.get_todo(db, todo_id)
     if not todo:
         raise HTTPException(status_code=404, detail="ToDo not found")
     return todo
 
-@app.get("/todos/filter/{completed}", response_model=list[schemas.ToDoInDB])
+@app.get("/todos/filter/{completed}", response_model=list[schemas.TodoInDB])
 def filter_todo(completed: bool, db: Session = Depends(get_db)):
     return crud.filter_todos(db, completed)
 
-@app.post("/todos", response_model=schemas.ToDoInDB)
+@app.post("/todos", response_model=schemas.TodoInDB)
 def create_todo(todo: schemas.ToDoCreate, db: Session = Depends(get_db)):
     return crud.create_todo(db, todo)
 
-@app.put("/todos/{todo_id}", response_model=schemas.ToDoInDB)
+@app.put("/todos/{todo_id}", response_model=schemas.TodoInDB)
 def update_todo(todo_id: int, todo: schemas.ToDoUpdate, db: Session = Depends(get_db)):
     return crud.update_todo(db, todo_id, todo)
 
