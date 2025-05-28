948608cd5dafc03a88f2a76d238f5f1b5b567692
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-05-28 23:38:58 +0800

    s

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "https://etodofasts.onrender.com";
+const API_URL = "https://etodofull.onrender.com";
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([]);
