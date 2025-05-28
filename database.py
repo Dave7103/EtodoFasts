9f1c48241cd42f9a50f9f1a4e22045fd201424c8
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-05-28 23:29:24 +0800

    s

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
-# app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = "postgresql://todofastsapi_user:X4n5Jbs0g9RcXFgcr4QdVAl2UxipaWor@dpg-d0qrb4p5pdvs73asd4h0-a.singapore-postgres.render.com/todofastsapi"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+database_url = os.environ.get("DATABASE_URL")
+engine = create_engine(database_url, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
